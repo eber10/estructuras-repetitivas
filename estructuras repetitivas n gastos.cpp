4d62a69f6834b8267d63f5e5bb3c6590981553e5
eber <eber.penaloza10@gmail.com>
2024-06-15 21:52:25 -0700

    mensaje de bienvenida

--- estructuras repetitivas n gastos.cpp
+++ estructuras repetitivas n gastos.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 int main()
 {
-int gasto;
-double suma=0;
-string resp;
-do
-{
-cout<<"Ingrese un gasto: "; cin>>gasto;
-cout<<"Desea seguir ingresando gastos (si/no)? : "; cin>>resp;
-suma=suma+gasto;
-}
-while(resp=="si");
-cout<<" gasto total: S/. " <<suma;
-return 0;
+    int gasto;
+    double suma=0;
+    string resp;
+    cout<<"BIENVENIDO AL SISTEMA"<<endl;
+    cout<<"*********************"<<endl;
+    do
+    {
+        cout<<"Ingrese un gasto: "; cin>>gasto;
+        cout<<"Desea seguir ingresando gastos (si/no)? : "; cin>>resp;
+        suma=suma+gasto;
+    }
+    while(resp=="si");
+    cout<<" gasto total: S/. " <<suma;
+    return 0;
 }
