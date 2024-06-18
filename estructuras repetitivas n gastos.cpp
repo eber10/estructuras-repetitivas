786ba303bf8513a743cafeba1c70dec5dbdfa9a9
eber <eber.penaloza10@gmail.com>
2024-06-17 17:32:11 -0700

    menú agregado

--- estructuras repetitivas n gastos.cpp
+++ estructuras repetitivas n gastos.cpp
@@ -3,20 +3,38 @@
 using namespace std;
 int main()
 {
-    int gasto;
+	int op;
+    double gasto;
     double suma=0;
     string resp, nombre;
     cout<<"BIENVENIDO AL SISTEMA"<<endl;
     cout<<"*********************"<<endl;
-    cout<<"Ingrese su nombre: "; cin>>nombre;
     do
     {
-        cout<<"Ingrese un gasto: "; cin>>gasto;
-        cout<<nombre<<" "<<"desea seguir ingresando gastos (si/no)? : "; cin>>resp;
-        suma=suma+gasto;
+    	cout<<"1. sumar "<<endl;
+    	cout<<"2. multiplicar "<<endl;
+    	cout<<"0. salir "<<endl;
+    	cin>>op;
+    	switch(op)
+    	{
+    		case 1:
+    			cout<<"Ingrese su nombre: "; cin>>nombre;
+    			do
+    			{
+    				cout<<"Ingrese un gasto: "; cin>>gasto;
+    				cout<<nombre<<" "<<"deseas seguir ingresando gastos (si/no) ? : "; cin>>resp;
+    				suma=suma+gasto;
+				}
+				while(resp=="si");
+				cout<<" gasto total: S/. " <<suma<<endl;
+				break;
+			case 2:
+				break;
+			default:
+				break;
+		}
     }
-    while(resp=="si");
-    cout<<" gasto total: S/. " <<suma<<endl;
+    while(op!=0);
     cout<<"GRACIAS POR USAR NUESTRO SISTEMA DE CALCULOS ";
     return 0;
 }
